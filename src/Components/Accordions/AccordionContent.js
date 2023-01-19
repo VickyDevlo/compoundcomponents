a1cfe9af03e88a7f272814d367f62e74f8163a11
Pranav Magare <vickymagare1996@gmail.com>
2023-01-19 10:52:23 +0530

    Add

--- src/Components/Accordions/AccordionContent.js
+++ src/Components/Accordions/AccordionContent.js
@@ -8,4 +8,3 @@ const AccordionContent = ({ children }) => {
 };
 
 export default AccordionContent;
-  
--- src/Components/Accordions/Header.js
+++ src/Components/Accordions/Header.js
@@ -1,19 +1,11 @@
 import React, { useContext } from "react";
 import { AccordionContext } from "./Accordion";
+import "./Header.css";
 
 const Header = ({ children }) => {
   const { clickHandler } = useContext(AccordionContext);
 
-  return (
-    <div
-      onClick={clickHandler}
-      style={{
-        display: "grid",
-      }}
-    >
-      {children}
-    </div>
-  );
+  return <div className="headerWrapper" onClick={clickHandler}>{children}</div>;
 };
 
 export default Header;
