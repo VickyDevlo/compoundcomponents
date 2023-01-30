693c98f7be6cb829dec833cf3498d5d927e7be4b
Pranav Magare <vickymagare1996@gmail.com>
2023-01-30 15:27:05 +0530

    add

--- src/Components/Tabs/Item.js
+++ src/Components/Tabs/Item.js
@@ -2,19 +2,19 @@ import React, { useContext } from "react";
 import { TabContext } from "./Tab";
 
 const Item = ({ children, index }) => {
-    const { onchange, currentTab } = useContext(TabContext);
-    const clickHandler = () => {
-        onchange(index);
-    };
+  const { onchange, currentTab } = useContext(TabContext);
+  const clickHandler = () => {
+    onchange(index);
+  };
 
-    return (
-        <div
-            className={index === currentTab ? "active tabItem" : "tabItem"}
-            onClick={clickHandler}
-        >
-            {children}
-        </div>
-    );
+  return (
+    <div
+      className={index === currentTab ? "active tabItem" : "tabItem"}
+      onClick={clickHandler}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Item;
